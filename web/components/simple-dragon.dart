86e0bb58e439464a24662cbecc19efe697255346
Ryan Gee <rgee@squarespace.com>
2013-12-14 21:16:57 -0500

    Add a hierarchical routing library.

--- web/components/simple-dragon.dart
+++ web/components/simple-dragon.dart
@@ -1,6 +1,17 @@
 import 'package:polymer/polymer.dart';
+import 'package:route_hierarchical/client.dart';
 
 @CustomTag('simple-dragon')
 class SimpleDragon extends PolymerElement {
-  SimpleDragon.created() : super.created();
+  final Router _router = new Router();
+  
+  SimpleDragon.created() : super.created() {
+    _router.root
+      ..addRoute(
+          name: 'partiesList',
+          path: '/parties',
+          defaultRoute: true);
+    _router.listen();
+    _router.go('partiesList', {});
+  }
 }
